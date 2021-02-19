a2d6826f8293f27bd6c2cc043b743fe099124cca
Hyounseok Ohe <hsohe74@gmail.com>
2021-02-19 10:54:10 +0900

    first

---
+++ src/main/java/com/hsohe/demo/HsoheSts001Application.java
@@ -0,0 +1,13 @@
+package com.hsohe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsoheSts001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsoheSts001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsohe/demo/HsoheSts001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsohe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsoheSts001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
